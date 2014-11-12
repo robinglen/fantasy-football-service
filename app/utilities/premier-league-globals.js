b43ee2f1715e671ae4cbd491336613dd529ad3ec
Robin Glen <robin@iamrobinglen.com>
2014-11-12 20:08:18 +0000

    started adding team mapping

--- app/utilities/premier-league-globals.js
+++ app/utilities/premier-league-globals.js
@@ -4,7 +4,89 @@ var premierLeague = {
         teams: 20,
         gameweeks: 38,
         transferCosts: 4
-    }
+    },
+    teams: [
+    	{
+    		name: 'Aresenal',
+    		shorthand: 'ars',
+    	},
+    	{
+    		name: 'Aston Villa',
+    		shorthand: 'avl',
+    	},
+    	{
+    		name: 'Burnley',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Chelsea',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Crystal Palace',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Everton',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Hull',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Leicester',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Liverpool',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Man City',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Man Utd',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Newcastle',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'QPR',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Southampton',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Spurs',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Stoke',
+    		shorthand: '??'
+    	},    	
+    	{
+    		name: 'Sunderland',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'Swansea', // 18
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'West Brom',
+    		shorthand: '??'
+    	},
+    	{
+    		name: 'West Ham',
+    		shorthand: '??'
+    	}      	
+    ]
 };
 
 
