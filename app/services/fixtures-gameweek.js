6f617972a8ff4780e91c5c60557ebbe3bf57485c
Robin Glen <robin@iamrobinglen.com>
2015-01-01 11:30:20 +0000

    extending date functionality for fixtures

--- app/services/fixtures-gameweek.js
+++ app/services/fixtures-gameweek.js
@@ -5,21 +5,27 @@ var config = require('../../config/config'),
  moment = require('moment');
 
 
+// I need to add the year into the date format
+// if aug to dec start year season
+// if after end year season
+
+// need build date functionality
 
 var responseGeneration = {
     buildgameweekFixturesResponse: function (cheerioBody) {
       var $ = cheerioBody,
           gamesInGameweekLength = $(gameweekFixturesSelectors.table).length,
           fixturesArr = [],
-          deadline = $(gameweekFixturesSelectors.title).text().split('- ')[1];
+          deadline = moment($(gameweekFixturesSelectors.title).text().split('- ')[1], 'DD MMM hh:mm').format('LLLL');
       for (i = 1; i <= gamesInGameweekLength; i++) {
         var fixtureRow = _.template(gameweekFixturesSelectors.row,{number:i}),
           fixtureRowSelector = gameweekFixturesSelectors.table + fixtureRow;
           // horrible case as couldn't find: $('table.ismFixtureTable tbody tr.ismFixture:nth-child(2)')
           if ($(fixtureRowSelector).hasClass(gameweekFixturesSelectors.fixture)) {
-          var date = $(fixtureRowSelector + gameweekFixturesSelectors.fixtures.date).text()
+          var date = moment($(fixtureRowSelector + gameweekFixturesSelectors.fixtures.date).text(), 'DD MMM hh:mm').format('LLLL');
           obj = {
             date: date,
+            kickoff: moment(date).format('HH:mm'),
             home: {
               club: clubDetails.searchForClubDetails($(fixtureRowSelector + gameweekFixturesSelectors.fixtures.home).text(), 'name'),
             },
@@ -34,15 +40,47 @@ var responseGeneration = {
           }
           fixturesArr.push(obj);
         }
-      }; 
+      };
+      var groupMatchesByDateObj = groupMatchesByDate(fixturesArr);
+      var flattenMatchesIntoMatchDayArr = flattenMatchesIntoMatchDay(groupMatchesByDateObj); 
       return {
         gameweek: Number($(gameweekFixturesSelectors.title).text().split('Gameweek ')[1].split(' -')[0]),
         deadline: deadline,
-        fixtures:fixturesArr 
+        fixtures:flattenMatchesIntoMatchDayArr 
       }
     }
 
   };
+
+function flattenMatchesIntoMatchDay(groupMatchesByDateObj) {
+  var collectMatchDaysTogetherArr = [];
+  var matchDayKeys = _.keys(groupMatchesByDateObj);
+  matchDayKeys.forEach(function(date){
+    var obj = {
+      matchDay: moment(date,'MMMDD').format('dddd MMMM Do'),
+      matches: groupMatchesByDateObj[date]
+    }
+    collectMatchDaysTogetherArr.push(obj);
+  })
+  return collectMatchDaysTogetherArr;
+} 
+
+
+function groupMatchesByDate(fixturesArr) {
+  var matchDayObj = {};
+  fixturesArr.forEach(function(match){
+    var shortDate = moment(match.date).format('MMMDD');
+    if (_.has(matchDayObj, shortDate)) {
+      matchDayObj[shortDate].push(match);
+    } else {
+      matchDayObj[shortDate] = [match];
+    }
+  })
+  return matchDayObj;
+} 
+
+
+
 
 function collectScoreIfAvailable(scoreline) {
   if (scoreline === 'v') {
